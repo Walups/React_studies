0fcba56a29bc0c5f8323c889c58cb2d64b1d4a23
Wallison lopes <wallison.lpss@gmail.com>
2025-05-15 23:24:20 -0300

    create bunner

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import Banner from './component/Banner'
+
+function App() {
+  
+  return (
+
+    <div>
+
+      <Banner />
+      
+
+    </div>
+    
+  )
+}
+
+export default App
---
+++ src/component/banner.jsx
@@ -0,0 +1,13 @@
+
+function Banner() {
+  return (
+    
+    <header className="text-center bg-[#6278F7]" >
+      <img className="inline-block max-w-full h-auto"  
+        src="./imagens/banner.png" alt="Banner principal da página"
+      />  
+    </header>
+  );
+}
+
+export default Banner;
